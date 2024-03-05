7e2892ef3944dd74f0eab4bb65fff6884fe249dd
seongRyoung <tjdfyd6619@gmail.com>
2024-03-05 13:33:50 +0900

    Setting: 라우터 경로 설정 세팅

--- client/src/app/App.tsx
+++ client/src/app/App.tsx
@@ -1,5 +1,12 @@
-function App() {
-  return <>안녕하세요</>;
-}
+import ReactDOM from 'react-dom/client';
+import React from 'react';
+import { RouterProvider } from 'react-router-dom';
+import { AppRouter } from './AppRouter';
 
-export default App;
+const root = document.getElementById('root') as HTMLElement;
+
+ReactDOM.createRoot(root).render(
+  <React.StrictMode>
+    <RouterProvider router={AppRouter()} />
+  </React.StrictMode>,
+);
---
+++ client/src/app/AppRouter.tsx
@@ -0,0 +1,106 @@
+import { Navigate, createBrowserRouter } from 'react-router-dom'; //라우터
+import CategoryView from '../pages/CategoryView';
+
+export const AppRouter = () =>
+  createBrowserRouter([
+    {
+      children: [
+        // 렌딩 페이지
+        {
+          path: '/',
+          element: (
+            <>
+              <div>렌딩페이지인디요 ?</div>
+            </>
+          ),
+        },
+        // 로그인 페이지
+        {
+          path: '/login',
+          element: (
+            <>
+              <div>로그인페이지인디요 ?</div>
+            </>
+          ),
+        },
+        // 회원가입 페이지
+        {
+          path: '/register',
+          element: (
+            <>
+              <div>회원가입페이진디요 ?</div>
+            </>
+          ),
+        },
+        // 마이 페이지
+        {
+          path: '/mypage/:userid_num',
+          element: (
+            <>
+              <div>마이페이지인디요 ?</div>
+            </>
+          ),
+        },
+        // 카테고리 페이지
+        {
+          path: '/categoryview/:userid_num',
+          element: (
+            <>
+              <div>카테고리 뷰인디요 ?</div>
+            </>
+          ),
+        },
+        // 노드 뷰 페이지
+        {
+          path: '/nodeview/:userid_num/:category',
+          element: (
+            <>
+              <div>노드뷰인디유 ?</div>
+            </>
+          ),
+        },
+        // 글쓰기 페이지
+        {
+          path: '/wirtenewpost/',
+          element: (
+            <>
+              <div>글쓰기페이진디유? ?</div>
+            </>
+          ),
+        },
+        // 리스트뷰 메인 페이지
+        {
+          path: '/listviewmain/:userid_num',
+          element: (
+            <>
+              <div>리스트뷰 메인 페이지?</div>
+            </>
+          ),
+        },
+        // 하나의 글 페이지
+        {
+          path: '/listviewdetail/:post_id',
+          element: (
+            <>
+              <div>하나의 글 페이진디유?</div>
+            </>
+          ),
+        },
+
+        // 검색 페이지
+        {
+          path: '/search',
+          element: (
+            <>
+              <div>검색 페이지인디유?</div>
+            </>
+          ),
+        },
+
+        {
+          path: '*',
+          element: <p>404에러페이지인디요?</p>,
+        },
+      ],
+    },
+  ]);
