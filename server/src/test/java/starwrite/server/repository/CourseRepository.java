605f98951e45ddfc03094eeac1e16d170feeecc7
minsuje <minsu.jex@gmail.com>
2024-03-07 19:52:36 +0900

    Feature : create Posts

--- server/src/test/java/starwrite/server/repository/CourseRepository.java
+++
@@ -1,7 +0,0 @@
-//package starwrite.server.repository;
-//
-//import org.springframework.data.neo4j.repository.Neo4jRepository;
-//import starwrite.server.dto.Course;
-//
-//public interface CourseRepository extends Neo4jRepository<Course,Long> {
-//}
