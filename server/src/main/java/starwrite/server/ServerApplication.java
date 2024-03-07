36ed48d4c87bf853900de4ea7a5feee5c6701a91
yewonkim301 <yeowne301@gmail.com>
2024-03-07 20:07:20 +0900

    feat: add userEntity and userRepository

--- server/src/main/java/starwrite/server/ServerApplication.java
+++ server/src/main/java/starwrite/server/ServerApplication.java
@@ -2,25 +2,16 @@ package starwrite.server;
 
 import org.neo4j.cypherdsl.core.renderer.Configuration;
 import org.neo4j.cypherdsl.core.renderer.Dialect;
-import org.neo4j.driver.Driver;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.data.neo4j.config.EnableNeo4jAuditing;
-import org.springframework.data.neo4j.core.ReactiveDatabaseSelectionProvider;
-import org.springframework.data.neo4j.core.transaction.ReactiveNeo4jTransactionManager;
-import org.springframework.data.neo4j.repository.config.EnableNeo4jRepositories;
-import org.springframework.data.neo4j.repository.config.ReactiveNeo4jRepositoryConfigurationExtension;
-import org.springframework.transaction.ReactiveTransactionManager;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 
 @SpringBootApplication
 @EnableJpaAuditing
-// @EnableNeo4jAuditing
-// @EnableNeo4jRepositories(basePackages = {"com.spring.neo4j.repository"})
 public class ServerApplication {
 	private final static Logger log = LoggerFactory.getLogger(ServerApplication.class);
 
