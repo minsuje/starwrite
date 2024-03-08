9f159c3cf1490d67da6664a835467f82b7c3cff0
KwonKuwhi <rngnl514@gmail.com>
2024-03-08 15:44:50 +0900

    setting: 라우터 outer추가해서 수정

---
+++ client/src/pages/ListView/ListViewPage.tsx
@@ -0,0 +1,7 @@
+import ListViewWid from '../../widgets/ListView/ListViewWid';
+
+function ListView({ content }: { content: string }) {
+  return <ListViewWid content={content}></ListViewWid>;
+}
+
+export default ListView;
