28c5b46eb6a59ab09c555d0a0e77066ab1cf8ce6
KwonKuwhi <rngnl514@gmail.com>
2024-03-08 20:09:02 +0900

    feat: ListViewPage 기본 레이아웃 생성

--- client/src/entities/ListView/ui/ListViewMainEnt.tsx
+++ client/src/entities/ListView/ui/ListViewMainEnt.tsx
@@ -1,5 +1,16 @@
-function ListViewMainEnt({ category }: { category: string }) {
-  return <>{category}</>;
+import { useParams } from 'react-router';
+import { ListHeaderEnt } from '..';
+
+function ListViewMainEnt() {
+  // 여기에 글 리스트 불러오면 됨
+  const { category } = useParams();
+  console.log(category);
+  return (
+    <>
+      {category} 카테고리 글 리스트
+      <ListHeaderEnt />
+    </>
+  );
 }
 
 export default ListViewMainEnt;
--- client/src/widgets/ListView/ListViewWid.tsx
+++ client/src/widgets/ListView/ListViewWid.tsx
@@ -1,34 +1,46 @@
 import styled from 'styled-components';
+import { ListCategory } from '../../entities/ListView';
+// import ListViewContentWid from './ListViewContentWid';
+import { Outlet } from 'react-router';
 
-import { useState } from 'react';
-import { ListCategory, ListViewMainEnt } from '../../entities/ListView';
-import ListViewContentWid from './ListViewContentWid';
+import { OneCategory } from '../../entities/ListView/ui/style';
 
 //sidebar + contentBox
 const _ListViewBox = styled.div`
   display: flex;
+  height: 100vh;
 `;
 
 const _CategoryBar = styled.div`
   width: 30%;
+  height: 85vh;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
 `;
 const _CategoryContent = styled.div`
   width: 100%;
-  background-color: #540000;
+  background-color: #1f1f1f;
+  height: 85vh;
+`;
+
+const _AddCategoryButton = styled(OneCategory)`
+  background-color: #3c3c3cb9;
 `;
 
 function ListViewWid({ content }: { content: string }) {
-  const [selected, setSelected] = useState<string>('전체');
-  console.log(selected);
   if (content === 'main') {
     return (
       <_ListViewBox>
         <_CategoryBar>
-          <ListCategory changeCategory={setSelected}></ListCategory>
+          <ListCategory></ListCategory>
+          <_AddCategoryButton>추가</_AddCategoryButton>
         </_CategoryBar>
 
         <_CategoryContent>
-          <ListViewContentWid selected={selected} />
+          {/* <ListViewContentWid selected={selected} /> */}
+          <Outlet />
         </_CategoryContent>
       </_ListViewBox>
     );
