f1caa9aadff9091b6d773bba44fcab97f6c07bc4
seejnn <seejinkim@gmail.com>
2024-03-17 18:33:23 +0900

    feature: add CORS config

--- server/src/main/java/starwrite/server/config/SecurityConfig.java
+++ server/src/main/java/starwrite/server/config/SecurityConfig.java
@@ -38,16 +38,16 @@ public class SecurityConfig {
 
 
   // ⭐️ CORS 설정
-  CorsConfigurationSource corsConfigurationSource() {
-    return request -> {
-      CorsConfiguration config = new CorsConfiguration();
-      config.setAllowedHeaders(Collections.singletonList("*"));
-      config.setAllowedMethods(Collections.singletonList("*"));
-      config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:5173")); // ⭐️ 허용할 origin
-      config.setAllowCredentials(true);
-      return config;
-    };
-  }
+//  CorsConfigurationSource corsConfigurationSource() {
+//    return request -> {
+//      CorsConfiguration config = new CorsConfiguration();
+//      config.setAllowedHeaders(Collections.singletonList("*"));
+//      config.setAllowedMethods(Collections.singletonList("*"));
+//      config.setAllowedOriginPatterns(Collections.singletonList("http://localhost:5173")); // ⭐️ 허용할 origin
+//      config.setAllowCredentials(true);
+//      return config;
+//    };
+//  }
 
 
     @Bean // securityFilterChain 통해서 HTTP 보안에 엑세스 할 수 있음
