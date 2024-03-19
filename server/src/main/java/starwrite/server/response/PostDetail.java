0f6fcd1ff8674e6f8995f80a916f6bc5fe4addd5
seejnn <seejinkim@gmail.com>
2024-03-20 05:10:14 +0900

    feature: add get post detail

--- server/src/main/java/starwrite/server/response/PostDetail.java
+++ server/src/main/java/starwrite/server/response/PostDetail.java
@@ -3,25 +3,34 @@ package starwrite.server.response;
 import java.time.LocalDateTime;
 import java.util.List;
 import lombok.Data;
-import starwrite.server.entity.Category;
+import starwrite.server.entity.Annotation;
 import starwrite.server.entity.Post;
-import starwrite.server.entity.Users;
 
 @Data
-// post 정보와 relationShip 타입만 가져오기
-public class GetPosts {
-//  private List<Post> post;
+public class PostDetail {
   private Long postIdentifier;
-  private String postTitle;
+
+  private String title;
+
   private String content;
+
   private String visible;
+
   private String img;
+
+  private boolean tmpSave;
+
   private LocalDateTime recentView;
+
   private LocalDateTime createdAt;
+
   private LocalDateTime updatedAt;
 
+  private String authorUserId;
+
+  private String authorNickname;
+
   private String categoryId;
-  private String categoryName;
-  private String userId;
-  private String userNickname;
+
+  private List<Annotation> annotations;
 }
