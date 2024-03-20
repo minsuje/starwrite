6086c8a6653a0d7b312dbdf37cbc695fa32abc4a
seejnn <seejinkim@gmail.com>
2024-03-20 15:31:46 +0900

    feature: add recentView to node view

--- server/src/main/java/starwrite/server/dto/PostTitleDTO.java
+++ server/src/main/java/starwrite/server/dto/PostTitleDTO.java
@@ -1,9 +1,11 @@
 package starwrite.server.dto;
 
+import java.time.LocalDateTime;
 import lombok.Data;
 
 @Data
 public class PostTitleDTO {
   private String title;
   private Long postId;
+  private LocalDateTime recentView;
 }
