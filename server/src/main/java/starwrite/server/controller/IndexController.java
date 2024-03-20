4a9b95e04106d1bcffd3f4673ab901f6e24d10b4
seejnn <seejinkim@gmail.com>
2024-03-20 16:09:44 +0900

    feature: add nickname when login

--- server/src/main/java/starwrite/server/controller/IndexController.java
+++ server/src/main/java/starwrite/server/controller/IndexController.java
@@ -1,5 +1,7 @@
 package starwrite.server.controller;
 
+import jakarta.servlet.http.Cookie;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
@@ -20,52 +22,66 @@ import starwrite.server.service.UsersServiceImpl;
 @RestController
 @RequiredArgsConstructor
 public class IndexController {
-    private final Logger Logger = LoggerFactory.getLogger(IndexController.class.getName());
 
-    @Autowired
-    UsersService usersService;
+  private final Logger Logger = LoggerFactory.getLogger(IndexController.class.getName());
 
-    @Autowired
-    UsersServiceImpl usersServiceimpl;
+  @Autowired
+  UsersService usersService;
 
-    @GetMapping("/home")
-    public String handleWelcome() {
-        return "home";
-    }
+  @Autowired
+  UsersServiceImpl usersServiceimpl;
 
-    @GetMapping("/admin/home")
-    public String handleAdminHome() {
-        return "home_admin";
-    }
+  @Autowired
+  private HttpServletResponse response;
 
-    @GetMapping("/user/home")
-    public String handleUserHome() {
-        System.out.println("controller nickname >>> " + SecurityUtil.getCurrentUserNickname());
-        System.out.println("controller userId >>> " + SecurityUtil.getCurrentUserUserId());
-        System.out.println("controller auth >>> " + SecurityUtil.getCurrentUserAuth());
-        return "home_user";
-    }
+  @GetMapping("/home")
+  public String handleWelcome() {
+    return "home";
+  }
+
+  @GetMapping("/admin/home")
+  public String handleAdminHome() {
+    return "home_admin";
+  }
+
+  @GetMapping("/user/home")
+  public String handleUserHome() {
+    System.out.println("controller nickname >>> " + SecurityUtil.getCurrentUserNickname());
+    System.out.println("controller userId >>> " + SecurityUtil.getCurrentUserUserId());
+    System.out.println("controller auth >>> " + SecurityUtil.getCurrentUserAuth());
+    return "home_user";
+  }
 
 //    @GetMapping("/login")
 //    public String handleLogin() {
 //        return "custom_login";
 //    }
 
-    @PostMapping("/login/post")
-    public JwtDTO signIn(@RequestBody LogInDTO logInDTO) {
-        System.out.println("signin" + logInDTO);
-        String username = logInDTO.getMail();
-        String password = logInDTO.getPassword();
-        JwtDTO jwtDTO = usersServiceimpl.signIn(username, password);
-        System.out.println("jwtDTO >>>>>>>>>" + jwtDTO);
-        log.info("request username = {}, password = {}", username, password);
-        log.info("jwtDTO accessToken = {}, refreshToken = {}", jwtDTO.getAccessToken(), jwtDTO.getRefreshToken());
+  @PostMapping("/login/post")
+  public JwtDTO signIn(@RequestBody LogInDTO logInDTO) {
+    System.out.println("signin" + logInDTO);
+    String username = logInDTO.getMail();
+    String password = logInDTO.getPassword();
+    JwtDTO jwtDTO = usersServiceimpl.signIn(username, password);
+    System.out.println("jwtDTO >>>>>>>>>" + jwtDTO);
+    log.info("request username = {}, password = {}", username, password);
+    log.info("jwtDTO accessToken = {}, refreshToken = {}", jwtDTO.getAccessToken(),
+        jwtDTO.getRefreshToken());
 
-        return jwtDTO;
-    }
+    return jwtDTO;
+  }
 
-    @GetMapping("/test")
-    public Authentication authentication(Authentication authentication) {
-        return authentication;
-    }
+  @GetMapping("/cookie")
+  public Cookie setCookie(Authentication authentication) {
+    Cookie cookie = new Cookie("nickName", SecurityUtil.getCurrentUserNickname());
+    cookie.setMaxAge(60 * 60 * 24 * 7);  // 쿠키 유효 시간 : 1주일
+    response.addCookie(cookie);
+
+    return cookie;
+  }
+
+  @GetMapping("/test")
+  public Authentication authentication(Authentication authentication) {
+    return authentication;
+  }
 }
