ca0c2e04ebd7c30af822d0988b78ced705d52256
seongRyoung <tjdfyd6619@gmail.com>
2024-03-20 20:30:12 +0900

    feat 구글 로그인 테스트

---
+++ client/src/features/Login/google.tsx
@@ -0,0 +1,25 @@
+import { GoogleOAuthProvider, GoogleLogin } from '@react-oauth/google';
+import jwtDecode from 'jwt-decode';
+
+export function GoogleOAuth() {
+  const clientId =
+    '547835898042-k7ltqkia6kdspu0fjenn79jaenbrj6nj.apps.googleusercontent.com';
+
+  return (
+    <>
+      <GoogleOAuthProvider clientId='547835898042-k7ltqkia6kdspu0fjenn79jaenbrj6nj.apps.googleusercontent.com'>
+        <GoogleLogin
+          onSuccess={(credentialResponse) => {
+            // credential이 존재하는지 확인합니다.
+            if (credentialResponse.credential) {
+              console.log(jwtDecode(credentialResponse.credential));
+            }
+          }}
+          onError={() => {
+            console.log('Login Failed');
+          }}
+        />
+      </GoogleOAuthProvider>
+    </>
+  );
+}
