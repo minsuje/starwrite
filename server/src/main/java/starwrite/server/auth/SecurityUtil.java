026a7aa18b9f4cbdd8cabfcea673edbc7638444f
seejnn <seejinkim@gmail.com>
2024-03-22 17:53:41 +0900

    fix: login issue

--- server/src/main/java/starwrite/server/auth/SecurityUtil.java
+++ server/src/main/java/starwrite/server/auth/SecurityUtil.java
@@ -5,26 +5,33 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtil {
-    static final Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-    static HashMap<String, Object> userDetails = (HashMap<String, Object>) SecurityContextHolder.getContext().getAuthentication().getDetails();
-    public static String getCurrentUserNickname() {
-        if (authentication == null || authentication.getDetails() == null) {
-            throw new RuntimeException("No authentication information.");
-        }
-        return (String) userDetails.get("nickname");
+
+
+  public static String getCurrentUserNickname() {
+    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+    if (authentication == null || authentication.getDetails() == null) {
+      throw new RuntimeException("No authentication information.");
     }
+    HashMap<String, Object> userDetails = (HashMap<String, Object>) authentication.getDetails();
+    return (String) userDetails.get("nickname");
+  }
+
 
     public static String getCurrentUserUserId() {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getDetails() == null) {
             throw new RuntimeException("No authentication information.");
         }
+      HashMap<String, Object> userDetails = (HashMap<String, Object>) authentication.getDetails();
         return (String) userDetails.get("userId");
     }
 
     public static String getCurrentUserAuth() {
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
         if (authentication == null || authentication.getDetails() == null) {
             throw new RuntimeException("No authentication information.");
         }
+      HashMap<String, Object> userDetails = (HashMap<String, Object>) authentication.getDetails();
         return (String) userDetails.get("auth");
     }
 }
