6b70fa333450da20d433a16ee77a5d2b86c605a0
KwonKuwhi <rngnl514@gmail.com>
2024-03-23 10:10:18 +0900

    feat: categoriesSlice 생성

--- client/types/app.d.ts
+++ client/types/app.d.ts
@@ -2,24 +2,6 @@
 
 export type Search = string;
 
-export interface Category {
-  name: string;
-  categoryId: string;
-  created_at?: Date;
-  updated_at?: Date;
-}
-
-export interface Posts {
-  postId: number;
-  category_id: string;
-  content: string;
-  created_at?: Date;
-  updated_at?: Date;
-  title: string;
-  tmpSave: boolean;
-  public: string;
-  recentView: Date;
-}
 declare global {
   // eslint-disable-next-line @typescript-eslint/consistent-type-imports
   declare type RootState = import('../src/app/AppStore').RootState;
