7572d29547316b3f7cceca5ac44888903eb7cf3e
seejnn <seejinkim@gmail.com>
2024-03-31 20:04:36 +0900

    fix: linking type error

--- client/src/features/NewPost/lib/checkLinking.ts
+++ client/src/features/NewPost/lib/checkLinking.ts
@@ -1,14 +1,39 @@
 export default function checkLinking(check: string | undefined) {
   const titleArray: number[] = [];
 
+  type Props = {
+    postid?: string;
+    textColor?: string;
+    backgroundColor?: string;
+    textAlignment?: string;
+  };
+
+  type ContentObject = {
+    type: string;
+    props: Props;
+  };
+
+  type ArrayObject = {
+    id: string;
+    type: string;
+    props: Props;
+    content: ContentObject[];
+    children: ArrayObject[];
+  };
+
   if (check) {
-    const array = JSON.parse(check);
+    const array = JSON.parse(check) as ArrayObject[];
+    console.log('array >>>>>>>>>> ', array);
 
     for (let i = 0; i < array.length; i++) {
       for (let j = 0; j < array[i].content?.length; j++) {
-        if (array[i].content[j].type == 'mention') {
-          if (!titleArray.includes(Number(array[i].content[j].props.postid))) {
-            titleArray.push(Number(array[i].content[j].props.postid));
+        if (
+          array[i].content[j].type === 'mention' &&
+          array[i].content[j].props.postid
+        ) {
+          const postId = Number(array[i].content[j].props.postid);
+          if (!titleArray.includes(postId)) {
+            titleArray.push(postId);
           }
         }
       }
