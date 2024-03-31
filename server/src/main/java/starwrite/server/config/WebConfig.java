6bb3e8d110a43befb99ed78e3ba8c48f111bebef
seejnn <seejinkim@gmail.com>
2024-03-31 21:04:03 +0900

    fix: security cors

--- server/src/main/java/starwrite/server/config/WebConfig.java
+++ server/src/main/java/starwrite/server/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-            .allowedOrigins("http://localhost:8080", "http://localhost:5173")
+            .allowedOrigins("http://localhost:8080", "http://localhost:5173", "http://54.180.103.144")
             .allowedMethods("GET", "POST", "PATCH", "DELETE")
             .allowCredentials(true)
             .maxAge(3000);
